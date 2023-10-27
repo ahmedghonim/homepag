872142be59801d83fda92e2536f98c61bc86609f
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2023-10-27 19:53:53 +0200

    fix: 🔥 asdfasdf
    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -2,173 +2,177 @@ function App() {
   return (
     <div>
       <img src="/images/Frame-min.png" alt="Figma" className="absolute -z-10" />
-      <h1 className="pt-[50px] pr-[110px] md:pr-[450px] md:pt-[12%] m-auto text-5xl md:text-[170px] text-center text-white-A700_ab">
-        تطبيق المدرب <br />
-        الشخص
-      </h1>
-      <div className="mr-auto w-full m-auto">
-        <h2 className="text-xl  md:text-[64px] text-center text-white-A700_87">
-          تدريب وتغذية مع مدرب
+      <div className="opacity-0">
+        <h1 className="pt-[50px] pr-[110px] md:pr-[450px] md:pt-[12%] m-auto text-5xl md:text-[170px] text-center text-white-A700_ab">
+          تطبيق المدرب <br />
+          الشخص
+        </h1>
+        <div className="mr-auto w-full m-auto">
+          <h2 className="text-xl  md:text-[64px] text-center text-white-A700_87">
+            تدريب وتغذية مع مدرب
+          </h2>
+          <h2 className="text-xl md:text-[64px] md:mt-10 text-center text-white-A700_87">
+            شخصي مجانا
+          </h2>
+        </div>
+        <h2 className="text-xl md:pt-[16%] md:text-8xl text-center pt-[5%] text-white-A700">
+          ثبت التطبيق الان مجانا
         </h2>
-        <h2 className="text-xl md:text-[64px] md:mt-10 text-center text-white-A700_87">
-          شخصي مجانا
-        </h2>
-      </div>
-      <h2 className="text-xl md:pt-[16%] md:text-8xl text-center pt-[5%] text-white-A700">
-        ثبت التطبيق الان مجانا
-      </h2>
 
-      <p
-        className="mx-auto text-[17px] md:text-[60px] text-center text-white-A700 md:pt-[62%] pt-[62%]"
-        size="txtHarmattanBold70"
-      >
-        <span className="text-white-A700 font-harmattan font-bold">
-          تطبيق المدرب الشخصي ثوره في مجال التطبيقات{" "}
-        </span>
-        <span className="text-black-900 font-harmattan font-bold">
-          <>
-            الرياضية
-            <br />
-          </>
-        </span>
-      </p>
-
-      <div className="grid grid-cols-4 font-poppins md:gap-16 gap-5 justify-between md:pt-[16%] pt-[13%] w-full px-[5%]">
-        <span
-          className="text-[11px] md:text-[24px] text-center text-white-A700_87"
-          size="txtPoppinsRegular24"
+        <p
+          className="mx-auto text-[17px] md:text-[60px] text-center text-white-A700 md:pt-[62%] pt-[62%]"
+          size="txtHarmattanBold70"
         >
-          متابعة مع مدرب شخص في كل خطوة
-        </span>
-        <span
-          className="text-[8px] md:text-[23px] text-center text-white-A700_87"
-          size="txtPoppinsRegular24"
-        >
-          نظام تتبع سعرات ذكي طوال اليوم
-        </span>
-        <span
-          className="text-[8px] md:text-[23px] text-center text-white-A700_87"
-          size="txtPoppinsRegular24"
-        >
-          جداول تدريبية متنوعة سواءً في البيت او الصالة الرياضه{" "}
-        </span>
-        <span
-          className="text-[8px] md:text-[23px] text-center text-white-A700_87"
-          size="txtPoppinsRegular24"
-        >
-          انظمة غذائية متنوعه / كيتو / لوكارب /صيام متقطع ...
-        </span>
-      </div>
-      <h2 className="text-xl md:pt-[130px]  md:text-8xl text-center  text-white-A700">
-        ثبت التطبيق الان مجانا
-      </h2>
-      <div className="flex px-2">
-        <span className="text-2xl md:text-8xl flex flex-col md:gap-8 text-center text-white-A700 pt-[10%]">
           <span className="text-white-A700 font-harmattan font-bold">
-            متابعة تحت اشراف مدرب شخصي في اي{" "}
+            تطبيق المدرب الشخصي ثوره في مجال التطبيقات{" "}
           </span>
+          <span className="text-black-900 font-harmattan font-bold">
+            <>
+              الرياضية
+              <br />
+            </>
+          </span>
+        </p>
 
-          <span className="text-white-A700 font-harmattan font-bold">وقت</span>
-        </span>
-        <span className="mt-[10%] mr-[5%] md:text-5xl text-[11px]  text-right text-white-A700_99 md:tracking-[0.50px]">
-          من اول لحضة تقوم فيها بتثبيت التطبيق <br />
-          يقوم مدربك الشخصي بتوجيهك وتحديد افضل <br />
-          الطرق واسرعها واكثرها ملائمة لك حسب مستواك وهدفك مع المتابعة اليومية
-        </span>
-      </div>
-      <div className="md:mt-[90%] mt-[88%]">
-        <div
-          className="inset-x-[0] mx-auto md:text-5xl text-[11px] md:text-[44px] text-center text-white-A700  w-full"
-          size="txtHarmattanBold48"
-        >
-          حساب القيم الغذائية المناسبه لك باشراف المدرب الشخص
+        <div className="grid grid-cols-4 font-poppins md:gap-16 gap-5 justify-between md:pt-[16%] pt-[13%] w-full px-[5%]">
+          <span
+            className="text-[11px] md:text-[24px] text-center text-white-A700_87"
+            size="txtPoppinsRegular24"
+          >
+            متابعة مع مدرب شخص في كل خطوة
+          </span>
+          <span
+            className="text-[8px] md:text-[23px] text-center text-white-A700_87"
+            size="txtPoppinsRegular24"
+          >
+            نظام تتبع سعرات ذكي طوال اليوم
+          </span>
+          <span
+            className="text-[8px] md:text-[23px] text-center text-white-A700_87"
+            size="txtPoppinsRegular24"
+          >
+            جداول تدريبية متنوعة سواءً في البيت او الصالة الرياضه{" "}
+          </span>
+          <span
+            className="text-[8px] md:text-[23px] text-center text-white-A700_87"
+            size="txtPoppinsRegular24"
+          >
+            انظمة غذائية متنوعه / كيتو / لوكارب /صيام متقطع ...
+          </span>
         </div>
-        <div
-          className="bottom-[0] inset-x-[0] mx-auto md:text-5xl text-[11px] md:text-[44px] text-center text-white-A700 w-full"
-          size="txtHarmattanBold48"
-        >
-          مصممة حسب هدفك ومتغيرة حسب تقدمك{" "}
+        <h2 className="text-xl md:pt-[130px]  md:text-8xl text-center  text-white-A700">
+          ثبت التطبيق الان مجانا
+        </h2>
+        <div className="flex px-2">
+          <span className="text-2xl md:text-8xl flex flex-col md:gap-8 text-center text-white-A700 pt-[10%]">
+            <span className="text-white-A700 font-harmattan font-bold">
+              متابعة تحت اشراف مدرب شخصي في اي{" "}
+            </span>
+
+            <span className="text-white-A700 font-harmattan font-bold">
+              وقت
+            </span>
+          </span>
+          <span className="mt-[10%] mr-[5%] md:text-5xl text-[11px]  text-right text-white-A700_99 md:tracking-[0.50px]">
+            من اول لحضة تقوم فيها بتثبيت التطبيق <br />
+            يقوم مدربك الشخصي بتوجيهك وتحديد افضل <br />
+            الطرق واسرعها واكثرها ملائمة لك حسب مستواك وهدفك مع المتابعة اليومية
+          </span>
         </div>
-        <div
-          className="md:text-4xl text-[11px] text-center text-white-A700 top-[0]"
-          size="txtHarmattanBold36"
-        >
-          السعرات الحرارية{" "}
+        <div className="md:mt-[90%] mt-[88%]">
+          <div
+            className="inset-x-[0] mx-auto md:text-5xl text-[11px] md:text-[44px] text-center text-white-A700  w-full"
+            size="txtHarmattanBold48"
+          >
+            حساب القيم الغذائية المناسبه لك باشراف المدرب الشخص
+          </div>
+          <div
+            className="bottom-[0] inset-x-[0] mx-auto md:text-5xl text-[11px] md:text-[44px] text-center text-white-A700 w-full"
+            size="txtHarmattanBold48"
+          >
+            مصممة حسب هدفك ومتغيرة حسب تقدمك{" "}
+          </div>
+          <div
+            className="md:text-4xl text-[11px] text-center text-white-A700 top-[0]"
+            size="txtHarmattanBold36"
+          >
+            السعرات الحرارية{" "}
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col md:gap-[9px] justify-start md:pt-[15%] pt-[8%] w-[52%] ">
-        <div className="md:h-[45px] ml-[2%] relative w-[72%]">
-          <p className="inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max">
-            <div>جداول تدريب في الجيم مخصصه</div>
-            <div className="-mt-6 md:mt-0">مشروحة بالفيديو</div>
-          </p>
-        </div>
-        <div className="flex flex-col md:mt-4 mt-2 items-start justify-start w-full">
-          <div className="md:h-[46px] ml-[20%] md:mt-[137px] relative w-[72%]">
-            <p className="md:pt-8 mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max">
-              <div>جداول تدريبية في المنزل مخصصة</div>
-              <div className="-mt-6 md:mt-0">حسب مستواك وهدفك</div>
+        <div className="flex flex-col md:gap-[9px] justify-start md:pt-[15%] pt-[8%] w-[52%] ">
+          <div className="md:h-[45px] ml-[2%] relative w-[72%]">
+            <p className="inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max">
+              <div>جداول تدريب في الجيم مخصصه</div>
+              <div className="-mt-6 md:mt-0">مشروحة بالفيديو</div>
             </p>
           </div>
-          <p className=" md:mt-4 text-4xl text-[8px] md:text-[34px] text-right text-white-A700"></p>
-          <div className="md:h-[45px] ml-[2%] mt-2 relative w-[72%]">
-            <p className="md:pt-[200px] inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max">
-              <div>خصص جدولك التدريبي بنفسك</div>
-              <div className="-mt-6 md:mt-0">مع مكتبه ضخمه من التمارين</div>
-            </p>
+          <div className="flex flex-col md:mt-4 mt-2 items-start justify-start w-full">
+            <div className="md:h-[46px] ml-[20%] md:mt-[137px] relative w-[72%]">
+              <p className="md:pt-8 mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max">
+                <div>جداول تدريبية في المنزل مخصصة</div>
+                <div className="-mt-6 md:mt-0">حسب مستواك وهدفك</div>
+              </p>
+            </div>
+            <p className=" md:mt-4 text-4xl text-[8px] md:text-[34px] text-right text-white-A700"></p>
+            <div className="md:h-[45px] ml-[2%] mt-2 relative w-[72%]">
+              <p className="md:pt-[200px] inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max">
+                <div>خصص جدولك التدريبي بنفسك</div>
+                <div className="-mt-6 md:mt-0">مع مكتبه ضخمه من التمارين</div>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="md:pt-[20%] md:pr-[18%] pr-[18%] md:gap-5 flex flex-col gap-1 mt-[25%] ">
-        <div
-          className="md:right-[11%] md:text-[38px] text-[11px] text-right text-white-A700 "
-          size="txtHarmattanBold40"
-        >
-          جميع الانظمة الغذائية التي تخطر على بالك
-        </div>
-        <div
-          className="md:text-[38px] text-[11px] text-right text-white-A700 "
-          size="txtHarmattanBold40"
-        >
-          كيتو / اعتيادي / لوكارب / بدون حرمان
-        </div>
-        <div className="  md:right-[18%] md:text-[38px] text-[11px] text-right text-white-A700 top-[23%]">
-          مكتبه ضخمة لجميع المواد الغذئية{" "}
-        </div>
+        <div className="md:pt-[20%] md:pr-[18%] pr-[18%] md:gap-5 flex flex-col gap-1 mt-[25%] ">
+          <div
+            className="md:right-[11%] md:text-[38px] text-[11px] text-right text-white-A700 "
+            size="txtHarmattanBold40"
+          >
+            جميع الانظمة الغذائية التي تخطر على بالك
+          </div>
+          <div
+            className="md:text-[38px] text-[11px] text-right text-white-A700 "
+            size="txtHarmattanBold40"
+          >
+            كيتو / اعتيادي / لوكارب / بدون حرمان
+          </div>
+          <div className="  md:right-[18%] md:text-[38px] text-[11px] text-right text-white-A700 top-[23%]">
+            مكتبه ضخمة لجميع المواد الغذئية{" "}
+          </div>
 
-        <div
-          className="md:text-[38px] text-[11px] text-right text-white-A700 top-[34%]"
-          size="txtHarmattanBold40"
-        >
-          محسوبة العناصر الغذائية
+          <div
+            className="md:text-[38px] text-[11px] text-right text-white-A700 top-[34%]"
+            size="txtHarmattanBold40"
+          >
+            محسوبة العناصر الغذائية
+          </div>
         </div>
+        <h2 className="text-xl md:pt-[53%]  md:text-8xl text-center pt-[50%] text-white-A700">
+          ثبت التطبيق الان مجانا
+        </h2>
+        <div className="flex flex-col items-center justify-start pt-[57%] pr-[15%] md:gap-10">
+          <div
+            className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
+            size="txtHarmattanBold40"
+          >
+            تابع تقدمك واحسب نتائجك واهدافك{" "}
+          </div>
+          <div
+            className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
+            size="txtHarmattanBold40"
+          >
+            شاهد تقدمك ومستواك عبر الاحصائيات اليوميه والشهريه
+          </div>
+          <div
+            className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
+            size="txtHarmattanBold40"
+          >
+            راقب تقدم اوزانك في التدريب واحسب تطور الوزن في كل جلسة
+          </div>
+        </div>
+        <h2 className="text-xl md:pt-[23%]  md:text-8xl text-center pt-[30%] text-white-A700">
+          ثبت التطبيق الان مجانا
+        </h2>
       </div>
-      <h2 className="text-xl md:pt-[53%]  md:text-8xl text-center pt-[50%] text-white-A700">
-        ثبت التطبيق الان مجانا
-      </h2>
-      <div className="flex flex-col items-center justify-start pt-[57%] pr-[15%] md:gap-10">
-        <div
-          className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
-          size="txtHarmattanBold40"
-        >
-          تابع تقدمك واحسب نتائجك واهدافك{" "}
-        </div>
-        <div
-          className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
-          size="txtHarmattanBold40"
-        >
-          شاهد تقدمك ومستواك عبر الاحصائيات اليوميه والشهريه
-        </div>
-        <div
-          className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
-          size="txtHarmattanBold40"
-        >
-          راقب تقدم اوزانك في التدريب واحسب تطور الوزن في كل جلسة
-        </div>
-      </div>
-      <h2 className="text-xl md:pt-[23%]  md:text-8xl text-center pt-[30%] text-white-A700">
-        ثبت التطبيق الان مجانا
-      </h2>
     </div>
   );
 }
