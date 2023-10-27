21dad75523c97c747969676c81e78e04dcaf1ae6
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2023-10-27 19:05:43 +0200

    fix: 🚑️ asdfasdf

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <img src="/images/Frame-min.png" alt="Figma" className="absolute -z-10" />
-      <h1 className="pt-[50px] pr-[110px] md:pr-[450px] md:pt-[190px] m-auto text-5xl md:text-[170px] text-center text-white-A700_ab">
+      <h1 className="pt-[50px] pr-[110px] md:pr-[450px] md:pt-[16%] m-auto text-5xl md:text-[170px] text-center text-white-A700_ab">
         تطبيق المدرب <br />
         الشخص
       </h1>
@@ -33,9 +33,9 @@ function App() {
         </span>
       </p>
 
-      <div className="grid grid-cols-4 font-poppins md:gap-16 gap-5 items-center justify-between md:pt-[200px] pt-[13%] w-full px-[5%]">
+      <div className="grid grid-cols-4 font-poppins md:gap-16 gap-5 justify-between md:pt-[200px] pt-[13%] w-full px-[5%]">
         <span
-          className="text-[8px] md:text-[24px] text-center text-white-A700_87"
+          className="text-[11px] md:text-[24px] text-center text-white-A700_87"
           size="txtPoppinsRegular24"
         >
           متابعة مع مدرب شخص في كل خطوة
@@ -62,24 +62,21 @@ function App() {
       <h2 className="text-xl md:pt-[130px]  md:text-8xl text-center pt-10 text-white-A700">
         ثبت التطبيق الان مجانا
       </h2>
-      <div className="flex px-5">
-        <span
-          className="text-2xl md:text-8xl flex flex-col md:gap-8 text-center text-white-A700 pt-[15%]"
-          size="txtHarmattanRegular96"
-        >
+      <div className="flex px-2">
+        <span className="text-2xl md:text-8xl flex flex-col md:gap-8 text-center text-white-A700 pt-[10%]">
           <span className="text-white-A700 font-harmattan font-bold">
             متابعة تحت اشراف مدرب شخصي في اي{" "}
           </span>
 
           <span className="text-white-A700 font-harmattan font-bold">وقت</span>
         </span>
-        <span className="mt-[15%] mr-[5%] md:text-5xl text-[11px]  text-right text-white-A700_99 md:tracking-[0.50px]">
+        <span className="mt-[10%] mr-[5%] md:text-5xl text-[11px]  text-right text-white-A700_99 md:tracking-[0.50px]">
           من اول لحضة تقوم فيها بتثبيت التطبيق <br />
           يقوم مدربك الشخصي بتوجيهك وتحديد افضل <br />
           الطرق واسرعها واكثرها ملائمة لك حسب مستواك وهدفك مع المتابعة اليومية
         </span>
       </div>
-      <div className="flex flex-col md:gap-[9px] justify-start md:mt-0 md:pt-[1600px] pt-[110%] w-[52%] ">
+      <div className="flex flex-col md:gap-[9px] justify-start md:mt-0 md:pt-[1600px] pt-[112%] w-[52%] ">
         <div className="md:h-[45px]  ml-[2%] relative w-[72%]">
           <p className="inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max">
             <div>جداول تدريب في الجيم مخصصه</div>
@@ -102,7 +99,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="md:pt-[30%] md:pr-[18%] pr-[18%] md:gap-5 flex flex-col gap-1 mt-[16%] ">
+      <div className="md:pt-[20%] md:pr-[18%] pr-[18%] md:gap-5 flex flex-col gap-1 mt-[25%] ">
         <div
           className="md:right-[11%] md:text-[38px] text-[11px] text-right text-white-A700 "
           size="txtHarmattanBold40"
@@ -126,7 +123,7 @@ function App() {
           محسوبة العناصر الغذائية
         </div>
       </div>
-      <div className="flex flex-col items-center justify-start pt-[116%] pr-[15%] md:gap-10">
+      <div className="flex flex-col items-center justify-start pt-[115%] pr-[15%] md:gap-10">
         <div
           className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
           size="txtHarmattanBold40"
