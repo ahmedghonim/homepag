906829241ac2e4fcf9d387d4d542490fadda828b
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2023-10-28 16:20:02 +0200

    fix: 🐛 fixxx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       {/* Hero Section */}
       <section className="relative">
-        <img src="/hero.png" alt="" className="absolute top-0 -z-10" />
+        <img src="/hero-min.png" alt="" className="absolute top-0 -z-10" />
         <div className="pt-[12%] pr-[43%]">
           <h1 className=" text-5xl md:text-[170px] text-center text-white-A700_ab">
             تطبيق المدرب <br />
@@ -18,18 +18,18 @@ function App() {
 
       {/* social  */}
       <div className="text-center flex flex-col items-center mt-[5%]">
-        <img src="socail.png" alt="" className="w-2/3 mx-auto" />
+        <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
 
-      <img src="/phones.png" className="-mt-[10%]" />
+      <img src="/phones-min.png" className="-mt-[10%]" />
 
       <section className="relative md:-mt-36 -mt-10">
         <img
           className="absolute -top-[95%] left-[7%] -z-10 w-[40%]"
-          src="/yellwo.png"
+          src="/yellwo-min.png"
           alt=""
         />
         <p className="mx-auto whitespace-nowrap text-[17px] md:text-[60px] text-center text-white-A700 ">
@@ -43,13 +43,13 @@ function App() {
       </section>
 
       <div className="text-center flex flex-col items-center mt-[5%]">
-        <img src="socail.png" alt="" className="w-2/3 mx-auto" />
+        <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
       <section className="relative mt-10 h-fit">
-        <img src="/fllow.png" alt="" className="absolute top-0" />
+        <img src="/fllow-min.png" alt="" className="absolute top-0" />
         <div className="grid grid-cols-2 gap-5 w-[95%]">
           <span className="text-2xl md:text-8xl flex flex-col md:gap-8 text-center mt-[5%] text-white-A700">
             <span className="text-white-A700 font-harmattan font-bold">
@@ -60,7 +60,7 @@ function App() {
               وقت
               <img
                 className="absolute -top-[50%] left-[20%] -z-10 w-[80%]"
-                src="/orang.png"
+                src="/orang-min.png"
                 alt=""
               />
             </span>
@@ -74,7 +74,7 @@ function App() {
       </section>
       <section className="relative mt-[55%]">
         <img
-          src="/persentage.png"
+          src="/persentage-min.png"
           className="absolute right-0 left-0 w-full -z-10"
         />
         <div
@@ -97,11 +97,14 @@ function App() {
         </div>
       </section>
       <section className="relative">
-        <img src="/rightleft.png" className="absolute w-[97%] right-0 -z-10" />
+        <img
+          src="/rightleft-min.png"
+          className="absolute w-[97%] right-0 -z-10"
+        />
         <div className="flex flex-col md:gap-[50px] gap-2 justify-start pt-[15%]  w-[52%] ">
           <p className="inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max  bg-gray-500 bg-opacity-20 rounded-xl md:px-10 md:py-4 px-3 ml-[10%]">
             <img
-              src="/fitness.png"
+              src="/fitness-min.png"
               alt=""
               className="ml-auto md:pb-3 -mb-3 md:mb-0 w-[25%] md:w-auto"
             />
@@ -111,7 +114,7 @@ function App() {
 
           <p className="md:pt-8 mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max bg-gray-500 bg-opacity-20 rounded-xl md:px-10 md:py-4 px-3 ">
             <img
-              src="/home.png"
+              src="/home-min.png"
               alt=""
               className="ml-auto md:pb-3 w-[15%] -mb-3 md:mb-0  md:w-auto"
             />
@@ -121,7 +124,7 @@ function App() {
 
           <p className="inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max  bg-gray-500 bg-opacity-20 rounded-xl md:px-10 md:py-4 px-3 ml-[10%]">
             <img
-              src="/add.png"
+              src="/add-min.png"
               alt=""
               className="ml-auto md:pb-3 w-[25%] -mb-3 md:mb-0  md:w-auto"
             />
@@ -131,13 +134,13 @@ function App() {
         </div>
       </section>
       <div className="text-center flex flex-col items-center mt-[10%]">
-        <img src="socail.png" alt="" className="w-2/3 mx-auto" />
+        <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
       <section className="-mt-[15%]">
-        <img src="/food.png" className="w-full absolute -z-10" />
+        <img src="/food-min.png" className="w-full absolute -z-10" />
         <div className="pt-[25%] pr-[12%] md:gap-5 flex flex-col gap-1 ">
           <div
             className="md:right-[11%] md:text-[38px] text-[11px] text-right text-white-A700 "
@@ -165,14 +168,14 @@ function App() {
       </section>
 
       <div className="text-center flex flex-col items-center mt-[45%]">
-        <img src="socail.png" alt="" className="w-2/3 mx-auto" />
+        <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
       <section className="relative -mt-[10%]">
-        <img src="/Component_7.png" className="absolute w-full -z-10" />
-        <div className="flex flex-col items-center justify-start pt-[67%] pr-[15%] md:gap-10">
+        <img src="/Component_7-min.png" className="absolute w-full -z-10" />
+        <div className="flex flex-col items-center justify-start pt-[70%] pr-[15%] md:gap-10">
           <div
             className="md:text-[38px] text-[11px] text-right text-white-A700 w-full"
             size="txtHarmattanBold40"
@@ -195,7 +198,7 @@ function App() {
       </section>
 
       <div className="text-center flex flex-col items-center mt-[20%]">
-        <img src="socail.png" alt="" className="w-2/3 mx-auto" />
+        <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
