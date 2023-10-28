03f57eb18aa47a1f1fc358d657657909baf00b19
Joe <eljoe1717@gmail.com>
2023-10-28 23:02:17 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import 'animate.css';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
+
+
 function App() {
+  
   return (
     <div>
       {/* Hero Section */}
-      <section className="relative">
+      <section className="relative animate__animated animate__backInLeft">
         <img src="/hero-min.png" alt="" className="absolute top-0 -z-10" />
         <div className="pt-[12%] pr-[43%]">
           <h1 className=" text-5xl md:text-[170px] text-center text-white-A700_ab">
@@ -15,17 +20,17 @@ function App() {
           </h1>
         </div>
       </section>
-
       {/* social  */}
-      <div className="text-center flex flex-col items-center mt-[5%]">
+      <div className="text-center flex flex-col items-center mt-[5%] animate__animated animate__backInRight">
         <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
-
+      <AnimationOnScroll animateOnce={true} animateIn="animate__bounceIn">
       <img src="/phones-min.png" className="-mt-[10%]" />
-
+      </AnimationOnScroll>
+      <AnimationOnScroll animateOnce={true} animateIn="animate__backInDown">
       <section className="relative md:-mt-36 -mt-10">
         <img
           className="absolute -top-[95%] left-[7%] -z-10 w-[40%]"
@@ -41,21 +46,24 @@ function App() {
           </span>
         </p>
       </section>
+        </AnimationOnScroll>
 
+        <AnimationOnScroll animateOnce={true} animateIn="animate__backInUp">
       <div className="text-center flex flex-col items-center mt-[5%]">
         <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
+      </AnimationOnScroll>
+      <AnimationOnScroll animateOnce={true} animateIn="animate__lightSpeedInRight">
       <section className="relative mt-10 h-fit">
         <img src="/fllow-min.png" alt="" className="absolute top-0" />
         <div className="grid grid-cols-2 gap-5 w-[95%]">
           <span className="text-2xl md:text-8xl flex flex-col md:gap-8 text-center mt-[5%] text-white-A700">
-            <span className="text-white-A700 font-harmattan font-bold">
-              متابعة تحت اشراف مدرب شخصي في اي{" "}
-            </span>
-
+              <span className="text-white-A700 font-harmattan font-bold">
+                متابعة تحت اشراف مدرب شخصي في اي{" "}
+              </span>
             <span className="text-white-A700 font-harmattan font-bold relative">
               وقت
               <img
@@ -72,6 +80,8 @@ function App() {
           </span>
         </div>
       </section>
+          </AnimationOnScroll>
+      <AnimationOnScroll animateOnce={true} animateIn="animate__lightSpeedInLeft">
       <section className="relative mt-[55%]">
         <img
           src="/persentage-min.png"
@@ -96,6 +106,8 @@ function App() {
           السعرات الحرارية{" "}
         </div>
       </section>
+        </AnimationOnScroll>
+        <AnimationOnScroll animateOnce={true} animateIn="animate__bounceIn">
       <section className="relative">
         <img
           src="/rightleft-min.png"
@@ -121,7 +133,6 @@ function App() {
             <div>جداول تدريبية في المنزل مخصصة</div>
             <div className="-mt-6 md:mt-0">حسب مستواك وهدفك</div>
           </p>
-
           <p className="inset-x-[0] mx-auto text-4xl text-[11px] md:text-[34px] text-right text-white-A700 w-max  bg-gray-500 bg-opacity-20 rounded-xl md:px-10 md:py-4 px-3 ml-[10%]">
             <img
               src="/add-min.png"
@@ -133,12 +144,17 @@ function App() {
           </p>
         </div>
       </section>
+          </AnimationOnScroll>
+      <AnimationOnScroll animateOnce={true} animateIn="animate__lightSpeedInDown">
       <div className="text-center flex flex-col items-center mt-[10%]">
         <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
+      </AnimationOnScroll>
+
+      <AnimationOnScroll animateOnce={true} animateIn="animate__lightSpeedInUp">
       <section className="-mt-[15%]">
         <img src="/food-min.png" className="w-full absolute -z-10" />
         <div className="pt-[25%] pr-[12%] md:gap-5 flex flex-col gap-1 ">
@@ -166,13 +182,20 @@ function App() {
           </div>
         </div>
       </section>
+      </AnimationOnScroll>
 
+
+      <AnimationOnScroll animateOnce={true} animateIn="animate__lightSpeedInRight">
       <div className="text-center flex flex-col items-center mt-[45%]">
         <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
+      </AnimationOnScroll>
+
+
+      <AnimationOnScroll animateOnce={true} animateIn="animate__lightSpeedInLeft">
       <section className="relative -mt-[10%]">
         <img src="/Component_7-min.png" className="absolute w-full -z-10" />
         <div className="flex flex-col items-center justify-start pt-[70%] pr-[15%] md:gap-10">
@@ -196,13 +219,16 @@ function App() {
           </div>
         </div>
       </section>
+      </AnimationOnScroll>
 
+      <AnimationOnScroll animateOnce={true} animateIn="animate__bounceIn">
       <div className="text-center flex flex-col items-center mt-[20%]">
         <img src="socail-min.png" alt="" className="w-2/3 mx-auto" />
         <h2 className="text-xl md:text-8xl text-center  text-white-A700">
           ثبت التطبيق الان مجانا
         </h2>
       </div>
+      </AnimationOnScroll>
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // open in port 3000
   server: {
-    port: 3000,
+    // port: 3000,
   },
   plugins: [react()],
 });
