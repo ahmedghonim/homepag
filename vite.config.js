8d803cc0a4276e498c9083f163b961927f42df20
Joe <eljoe1717@gmail.com>
2023-12-22 14:47:09 +0200

    Fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // open in port 3000
   server: {
-    // port: 3000,
+    port: 3000,
+    build: {
+      outDir: "build",
+    }  
   },
   plugins: [react()],
 });
